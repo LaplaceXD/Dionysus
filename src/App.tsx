0b549d916aef0462b53777058bb225b9b3b61256
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-09-10 16:59:25 +0800

    [Feat][Lex] Extracted the background container outside of Game

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Game } from "@/pages/TicTacToe";
 
 function App() {
-  return <Game />;
+  return (
+    <div className="w-screen h-screen bg-gray-900 px-12">
+      <Game />
+    </div>
+  );
 }
 
 export default App;
