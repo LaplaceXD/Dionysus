65fc5faf844d905097354f513d12463420673c9e
Jonh Alexis <ja.buotph@gmail.com>
2023-09-12 08:18:58 +0000

    [Fix][Lex] Fixed overflow issue due to max-width being 100vw

--- src/layouts/PageContainer.tsx
+++ src/layouts/PageContainer.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 function PageContainer({ children }: PropsWithChildren) {
   return (
-    <div className="min-w-[100dvw] min-h-[100dvh] bg-gray-900 px-12">
+    <div className="min-h-[100dvh] bg-gray-900">
       {children}
     </div>
   );
