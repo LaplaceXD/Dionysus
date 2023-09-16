0035d82b00561ccaaa14a2e6bd1d515c026e614a
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-09-16 16:50:56 +0800

    [Fix][Lex] Configured new styling for the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      trasparent: colors.transparent,
+      white: colors.white,
+      neutral: colors.slate,
+      primary: colors.purple,
+      secondary: colors.blue,
+      accent: colors.fuchsia,
+    },
   },
   plugins: [],
 };
