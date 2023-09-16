dbe8844aa3888ed328e06f7ac6249f0b01bf6a18
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-09-16 13:58:17 +0800

    [Misc][Lex] Added husky

--- src/layouts/PageContainer.tsx
+++ src/layouts/PageContainer.tsx
@@ -1,11 +1,7 @@
 import { PropsWithChildren } from "react";
 
 function PageContainer({ children }: PropsWithChildren) {
-  return (
-    <div className="min-h-[100dvh] bg-gray-900">
-      {children}
-    </div>
-  );
+  return <div className="min-h-[100dvh] bg-gray-900">{children}</div>;
 }
 
 export default PageContainer;
--- src/layouts/SectionLayout.tsx
+++ src/layouts/SectionLayout.tsx
@@ -16,17 +16,21 @@ function SectionLayout({
   endTitle = false,
 }: PropsWithChildren<SectionLayoutProps>) {
   return (
-    <div className={clsx("flex flex-col justify-center items-center gap-16 max-w-[1024px] w-[90%] m-auto",
-    isHorizontal && "lg:flex-row")}>
+    <div
+      className={clsx(
+        "flex flex-col justify-center items-center gap-16 max-w-[1024px] w-[90%] m-auto",
+        isHorizontal && "lg:flex-row"
+      )}
+    >
       {endTitle ? children : null}
 
       <article className="flex flex-col gap-2 sm:gap-4 flex-1 justify-center">
-        <h1 className="flex font-bold text-center text-3xl sm:text-5xl text-right w-full">
+        <h1 className="flex font-bold text-3xl sm:text-5xl text-right w-full">
           {title}
         </h1>
         <p className="text-white">{description}</p>
       </article>
-      
+
       {endTitle ? null : children}
     </div>
   );
