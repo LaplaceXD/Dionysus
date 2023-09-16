3ce49b9b9c5e643426e96229a9b8fd6f3ad6710d
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-09-16 20:13:21 +0800

    [Feat][Lex] Updated the layout of GameCard Component

--- src/pages/Home/components/GameCard.tsx
+++ src/pages/Home/components/GameCard.tsx
@@ -9,17 +9,19 @@ interface GameCardProps {
 
 function GameCard({ image, title, description, to = null }: GameCardProps) {
   return (
-    <article className="group relative overflow-hidden rounded-xl opacity-40 hover:opacity-100 hover:shadow-2xl hover:shadow-cyan-200/50 hover:cursor-pointer transition-all duration-300">
+    <article className="relative group rounded-xl hover:cursor-pointer transition-all duration-300 pb-6">
       <img
         src={image}
         alt={title}
-        className="object-cover w-fill aspect-video sm:aspect-[3/4] grayscale group-hover:grayscale-0 transition-all duration-300"
+        className="object-cover rounded-lg w-fill aspect-[2/1] sm:aspect-[3/2] grayscale group-hover:grayscale-0 group-hover:shadow-2xl group-hover:shadow-secondary-500/20  transition-all duration-300"
       />
-      <div className="absolute opacity-0 group-hover:opacity-100 transition-all duration-300 inset-0 w-full h-full flex flex-col justify-end p-6 bg-gradient-to-t from-gray-900 text-white">
-        <h2 className="font-bold text-xl">{title}</h2>
-        <p className="text-sm">{description}</p>
-        {to && <Link to={to} className="absolute w-full h-full" />}
-      </div>
+      <h3 className="mt-6 font-bold text-xl md:text-2xl text-neutral-300 group-hover:bg-gradient-to-r group-hover:from-secondary-400 group-hover:to-primary-500 group-hover:bg-clip-text group-hover:text-trasparent trnsition-colors duration-300">
+        {title}
+      </h3>
+      <p className="text-sm md:text-base text-neutral-300 group-hover:bg-gradient-to-r group-hover:from-secondary-400 group-hover:to-primary-500 group-hover:bg-clip-text group-hover:text-trasparent trnsition-colors duration-300">
+        {description}
+      </p>
+      {to && <Link to={to} className="absolute w-full h-full bottom-0" />}
     </article>
   );
 }
