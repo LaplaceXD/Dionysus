b487faab944f5542a9f4948cda26bb54c011c36b
Jonh Alexis <ja.buotph@gmail.com>
2023-09-21 06:16:54 +0000

    [Feat][Lex] Created Field component and moved css for inputs to tailwind css

---
+++ src/components/Input/Field.tsx
@@ -0,0 +1,81 @@
+import clsx from "clsx";
+import {
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  ReactNode,
+  useId,
+  useMemo,
+} from "react";
+
+type InputControlledProps = Pick<
+  DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>,
+  "aria-describedby" | "aria-errormessage" | "aria-invalid" | "id"
+>;
+
+interface FieldProps {
+  id: string;
+  render: (field: InputControlledProps) => ReactNode;
+  label?: string;
+  className?: string;
+  description?: string;
+  error?: string;
+}
+
+function Field({
+  id,
+  render,
+  label,
+  className,
+  description,
+  error,
+}: FieldProps) {
+  const uniquePrefix = useId();
+  const uniqueId = uniquePrefix + id;
+
+  const fields = useMemo(() => {
+    const fields: InputControlledProps = { id };
+
+    if (description) {
+      fields["aria-describedby"] = uniqueId + "-descriptor";
+    }
+
+    if (error) {
+      fields["aria-errormessage"] = uniqueId + "-error";
+      fields["aria-invalid"] = Boolean(error);
+    }
+
+    return fields;
+  }, [uniqueId, description, error]);
+
+  return (
+    <div className={clsx("flex flex-col gap-1 text-white", className)}>
+      <label htmlFor={uniqueId} className="text-sm">
+        {label}
+      </label>
+
+      {render(fields)}
+
+      {description ? (
+        <p
+          id={fields["aria-describedby"]}
+          className="text-xs text-neutral-300 md:text-sm"
+        >
+          {description}
+        </p>
+      ) : null}
+
+      {error ? (
+        <p
+          role="alert"
+          id={fields["aria-errormessage"]}
+          className="text-xs text-error-400 md:text-sm"
+        >
+          {error}
+        </p>
+      ) : null}
+    </div>
+  );
+}
+
+export default Field;
+
--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,2 +1,3 @@
+export { default as Field } from "./Field";
 export { default as ImageUpload } from "./ImageUpload";
 export { default as InputField } from "./InputField";
