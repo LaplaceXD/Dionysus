357293d38348694e2b5897767e7447cd38b53838
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-09-22 17:15:21 +0800

    [Feat][Lex] Created shimmerable GameCard

--- src/pages/Games/components/GameCard.tsx
+++ src/pages/Games/components/GameCard.tsx
@@ -1,27 +1,75 @@
+import { Shimmer } from "@/components";
+import clsx from "clsx";
+import { useState } from "react";
 import { Link, To } from "react-router-dom";
 
 interface GameCardProps {
-  image: string;
-  title: string;
-  description: string;
+  image?: string;
+  title?: string;
+  description?: string;
   to?: To | null;
+  loading?: boolean;
 }
 
-function GameCard({ image, title, description, to = null }: GameCardProps) {
+function GameCard({
+  image,
+  title,
+  description,
+  loading = false,
+  to = null,
+}: GameCardProps) {
+  const [imgLoading, setImgLoading] = useState(true);
+
   return (
-    <article className="group relative rounded-xl pb-6 transition-all duration-300 hover:cursor-pointer">
-      <img
-        src={image}
-        alt={title}
-        className="w-fill aspect-[2/1] rounded-lg object-cover grayscale transition-all duration-300 group-hover:shadow-2xl group-hover:shadow-secondary-500/20  group-hover:grayscale-0 sm:aspect-[3/2]"
-      />
-      <h3 className="group-hover:text-gradient-primary mt-6 text-xl font-bold text-neutral-300 md:text-2xl">
-        {title}
-      </h3>
-      <p className="group-hover:text-gradient-primary text-sm text-neutral-300 md:text-base">
-        {description}
-      </p>
-      {to && <Link to={to} className="absolute bottom-0 h-full w-full" />}
+    <article
+      className={clsx(
+        "group relative rounded-xl pb-6 transition-all duration-300",
+        !loading && "hover:cursor-pointer"
+      )}
+    >
+      <Shimmer
+        loading={loading || imgLoading}
+        className="overflow-clip rounded-lg"
+      >
+        <img
+          src={image}
+          alt={title}
+          className={clsx(
+            "w-fill aspect-[2/1] rounded-lg object-cover grayscale transition-all duration-300 group-hover:shadow-2xl group-hover:shadow-secondary-500/20  group-hover:grayscale-0 sm:aspect-[3/2]",
+            (loading || imgLoading) && "invisible"
+          )}
+          onLoad={() => setImgLoading(false)}
+        />
+      </Shimmer>
+
+      <Shimmer
+        loading={loading}
+        className="w-fit min-w-[192px] overflow-clip rounded-md"
+      >
+        <h3
+          className={clsx(
+            "group-hover:text-gradient-primary text-xl font-bold text-neutral-300 md:text-2xl",
+            loading ? "invisible mt-4" : "mt-6"
+          )}
+        >
+          {title}
+        </h3>
+      </Shimmer>
+
+      <Shimmer loading={loading} className="overflow-clip rounded-md">
+        <p
+          className={clsx(
+            "group-hover:text-gradient-primary text-sm text-neutral-300 md:text-base",
+            loading && "invisible mt-2"
+          )}
+        >
+          {description}
+        </p>
+      </Shimmer>
+
+      {!loading && to && (
+        <Link to={to} className="absolute bottom-0 h-full w-full" />
+      )}
     </article>
   );
 }
